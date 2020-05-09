ee4f8658cdfdeae669834b96e4e11d117eb762c0
Ahmet Bohur <ahmetbohur@icloud.com>
2020-05-09 17:34:05 +0300

    Yeniden Kullanılabilir Sahne Değiştirme Scripti

---
+++ Unity/Yeniden Kullanılabilir Sahne Değiştirme Scripti/Assets/SahneDegistir.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SahneDegistir : MonoBehaviour
+{
+    
+    public void SahneDegistirr(int gidilecekSahne)
+    {
+        SceneManager.LoadScene(gidilecekSahne, LoadSceneMode.Single);
+    }
+  
+}
---
+++ Unity/Yeniden Kullanılabilir Sahne Değiştirme Scripti/Assets/Script2.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class Script2 : MonoBehaviour
+{
+    public GameObject sahneDegistirScript;
+
+    void SahneDegistirmeyeEris()
+    {
+        var sahneDegistir = sahneDegistirScript.GetComponent<SahneDegistir>();
+        sahneDegistir.SahneDegistirr(5);
+    }
+}
